49d302bd2d8c168f2461d126934307394873255d
viacheslavutrobin <viacheslav.utrobin@buddler.com>
2024-06-06 00:46:17 +0500

    part1 ch2

---
+++ part1/ch2/intro_numbers.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let twenty = 20;
+
+    let twenty_one: i32 = 21;
+    let twenty_two: 22i32;
+
+    let addition = twenty + twenty_one +twenty_two;
+
+    let one_million: i64 = 1_000_000;
+
+    let forty_twos = [
+        42.0,
+        42f32,
+        42.0_f32,
+    ];
+}
